cb1a90638096240edfbb7a2320a4598e4e9db40a
Mauricio Gonzalez <dreg5000.bbb@gmail.com>
2022-05-06 19:41:34 -0400

    validations_query 01

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 
 app = FastAPI()
@@ -29,3 +30,13 @@ def home():
 @app.post('/person/new')
 def create_person(person: Person = Body(...)):
     return person
+
+# Validations: Query Parameters
+# min_length and max_length both are query validators
+
+@app.get('/person/detail')
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    born_day: Optional[str] = Query(...),
+):
+    return {name: born_day}
